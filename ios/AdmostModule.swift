0dc832c13b06b3cd55eaec96246d18f23d3e3b0a
Sinan Elmalı <54123538+HTCZeus@users.noreply.github.com>
2023-08-31 12:22:39 +0300

    fix CCPA

--- ios/AdmostModule.swift
+++ ios/AdmostModule.swift
@@ -18,7 +18,7 @@ class AdmostModule: RCTEventEmitter {
     var appID: String? 
     var userConsents: Bool?
     var subjectToGDPR: Bool?
-    var setSubjectToCCPA: Bool?
+    var subjectToCCPA: Bool?
     var userChild: Bool?
     var setUserId: Bool?
   
